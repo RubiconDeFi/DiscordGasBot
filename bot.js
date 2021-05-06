0f16b6cb925a1ca728ee5e7075fccfc04daadd45
bghughes <benjamin@rubicon.finance>
2021-05-06 15:52:28 -0500

    pause

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('pong');
   } else if (String(msg.content).includes('gas')) {
-    getPrices().then(msg.reply('ETH gas is currently :', currentPrice, 'gwei'));
+    // getPrices().then(msg.reply('ETH gas is currently :' + String(currentPrice + " gwei")));
     return;
   } else if (String(msg.content).includes('wen moon')) {
     msg.reply('Soon :tm:');
