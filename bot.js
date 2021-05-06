5b76a9e4c66f42b477add3978ed0c0c55d90aad8
bghughes <benjamin@rubicon.finance>
2021-05-06 16:03:09 -0500

    bot boolean

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ client.on('ready', () => {
 client.on('message', msg => {
   console.log(msg.content);
   console.log(msg.author);
-  if (message.author.bot ==  true) return;
+  if (message.author.bot ==  true) {return;}
   if (msg.content === 'ping') {
     msg.reply('pong');
   } else if (String(msg.content).includes('gas') && msg.author.bot) {
